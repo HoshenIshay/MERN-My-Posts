5ca775410cb58279ecacea4541d79d1c518cff4a
Hoshen Ishay <87603302+HoshenIshay@users.noreply.github.com>
2022-11-09 19:40:01 +0200

    Update Posts.js

--- React client/src/Posts.js
+++ React client/src/Posts.js
@@ -1,139 +1,164 @@
-import React from 'react'
-import { useState , useEffect } from "react";
-import axios from "axios";
-import { Button , Form , Spinner } from "react-bootstrap";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { Button, Form } from "react-bootstrap";
 import Modal from "react-bootstrap/Modal";
 
 function Posts() {
-    const [posts, setPosts] = useState([]);
-    const navigate = useNavigate();
-    const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-    const [updatedPost, setUpdatedPost] = useState({
-        id: "",
-        title: "",
-        description: "",
-      });
+  const navigate = useNavigate();
+  const [posts, setPosts] = useState([]);
+  const [updatedPost, setUpdatedPost] = useState({
+    id: "",
+    title: "",
+    description: "",
+  });
+  const [show, setShow] = useState(false);
 
-    useEffect(() => {
-        axios
-          .get("https://posts-app-by-hoshen-ishay.herokuapp.com/posts")
-          .then((res) => {
-            setPosts(res.data);
-          })
-          .catch((err) => console.log(err));
-      }, []);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-      const deletePost = (id) => {
-        console.log(id)
-        axios
-          .delete(`https://posts-app-by-hoshen-ishay.herokuapp.com/delete/${id}`)
-          .then((res) => console.log(res))
-          .catch((err) => console.log(err));
-    
-        window.location.reload();
+  useEffect(() => {
+    axios
+      .get("/posts")
+      .then((res) => {
+        console.log(res);
+        setPosts(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const deletePost = (id) => {
+    console.log(id);
+
+    axios
+      .delete(`/delete/${id}`)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+
+    window.location.reload();
+  };
+
+  const updatePost = (id, title, description) => {
+    setUpdatedPost((prev) => {
+      return {
+        ...prev,
+        id: id,
+        title: title,
+        description: description,
       };
+    });
+    handleShow();
+  };
 
-      const updatePost = (id, title, description) => {
-        setUpdatedPost((prev) => {
-            return {
-              ...prev,
-              id: id,
-              title: title,
-              description: description,
-            };
-          });
-        handleShow();
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUpdatedPost((prev) => {
+      return {
+        ...prev,
+        [name]: value,
       };
+    });
+  };
 
-      const handleChange = (e) => {
-        const { name, value } = e.target;
-        setUpdatedPost((prev) => {
-          return {
-            ...prev,
-            [name]: value,
-          };
-        });
-      };
+  const saveUpdatedPost = () => {
+    console.log(updatedPost);
 
-      const saveUpdatedPost = () => {
-        console.log(updatedPost.id)
-        console.log(updatedPost)
-        axios
-          .put(`https://posts-app-by-hoshen-ishay.herokuapp.com/update/${updatedPost.id}`, updatedPost)
-          .then((res) => console.log(res))
-          .catch((err) => console.log(err));
-    
-        handleClose();
-        window.location.reload();
-      };
+    axios
+      .put(`/update/${updatedPost.id}`, updatedPost)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+
+    handleClose();
+    window.location.reload();
+  };
 
   return (
     <div style={{ width: "90%", margin: "auto auto", textAlign: "center" }}>
-        <h1>Posts</h1>
-        <Button
+      <h1>Posts page</h1>
+      <Button
         variant="outline-dark"
         style={{ width: "100%", marginBottom: "1rem" }}
-        onClick={() => navigate("/create")}>
-        NEW POST
+        onClick={() => navigate(-1)}
+      >
+        BACK
       </Button>
-      { !posts ? (
-         <Spinner animation="border" variant="primary" />
-            ) : ""}
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>Update a post</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-        <Form.Control
-            placeholder="Title"
+          <Form.Control
+            placeholder="title"
             name="title"
             value={updatedPost.title ? updatedPost.title : ""}
-            onChange={handleChange}
             style={{ marginBottom: "1rem" }}
-          />
-        <Form.Control
-            placeholder="Description"
-            name="description"
-            value={updatedPost.description ? updatedPost.description : ""}
             onChange={handleChange}
+          />
+          <Form.Control
+            placeholder="description"
+            name="description"
+            onChange={handleChange}
+            value={updatedPost.description ? updatedPost.description : ""}
           />
         </Modal.Body>
         <Modal.Footer>
-          <Button onClick={handleClose} variant="secondary" >
+          <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary" onClick={saveUpdatedPost} >
+          <Button variant="primary" onClick={saveUpdatedPost}>
             Save Changes
           </Button>
         </Modal.Footer>
       </Modal>
-        {
-            posts ? (
-                <>
-                {
-                 posts.map(post =>{
-                    return (
-                        <div
-                         key={post._id} 
-                         style={{ marginBottom: "1rem", border: "solid lightgray 1px", borderRadius: "8px" , padding: "1rem"}}>
-                            <h4>{post.title}</h4>
-                            <p>{post.description}</p>
-                            {post.imgUrl ?  <img src={post.imgUrl} style={{ height: "150px" , marginBottom: "1rem", width: "150px" , borderRadius: "8px" }} alt={post._id}/> : ''}
-                            <div  style={{ display: "flex" , flexDirection: "row" , justifyContent:"space-between"}}>
-                            <Button onClick={() => updatePost(post._id, post.title, post.description)} variant="outline-info" style={{ width: "100%", marginRight: "1rem" }}>UPDATE</Button>
-                            <Button onClick={() => deletePost(post._id)} variant="outline-danger" style={{ width: "100%" }}>DELETE</Button>    
-                            </div>
-                        </div>
-                    )
-                 })   
-                }
-                </>
-            ) : ""}
+      {posts ? (
+        <>
+          {posts.map((post) => {
+            return (
+              <div
+                style={{
+                  marginBottom: "1rem",
+                  border: "solid lightgray 1px",
+                  borderRadius: "8px",
+                }}
+                key={post._id}
+              >
+                <h4>{post.title}</h4>
+                <p>{post.description}</p>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+
+                    padding: "1rem",
+                  }}
+                >
+                  <Button
+                    variant="outline-info"
+                    onClick={() =>
+                      updatePost(post._id, post.title, post.description)
+                    }
+                    style={{ width: "100%", marginRight: "1rem" }}
+                  >
+                    UPDATE
+                  </Button>
+                  <Button
+                    onClick={() => deletePost(post._id)}
+                    variant="outline-danger"
+                    style={{ width: "100%" }}
+                  >
+                    DELETE
+                  </Button>
+                </div>
+              </div>
+            );
+          })}
+        </>
+      ) : (
+        ""
+      )}
     </div>
-  )
+  );
 }
 
-export default Posts
+export default Posts;
