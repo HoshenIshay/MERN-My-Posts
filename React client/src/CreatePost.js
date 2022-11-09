373a5c6387882c2826d387d833d52051024084ec
Hoshen Ishay <87603302+HoshenIshay@users.noreply.github.com>
2022-11-09 19:39:18 +0200

    Update CreatePost.js

--- React client/src/CreatePost.js
+++ React client/src/CreatePost.js
@@ -1,16 +1,13 @@
-import React from 'react'
-import { Button , Form} from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
-import { useState , useEffect } from "react";
+import { Form, Button } from "react-bootstrap";
+import { useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function CreatePost() {
-
   const navigate = useNavigate();
   const [post, setPost] = useState({
     title: "",
     description: "",
-    // imgUrl : ""
   });
 
   const handleChange = (e) => {
@@ -23,82 +20,54 @@ function CreatePost() {
     });
   };
 
-  const handleImgUrl = (e) => {
-    let files = e.target.files;
-    let reader = new FileReader();
-    reader.onload = r => {
-         setPost((prev) => {
-          const { name } = e.target;
-          return {
-            ...prev,
-            [name]: r.target.result,
-          };
-        });
-     };
-     console.log(post)
-    reader.readAsDataURL(files[0]);
-}
-
   const createPost = (e) => {
     e.preventDefault();
-    console.log(post)
+
     axios
-      .post("https://posts-app-by-hoshen-ishay.herokuapp.com/create", post)
+      .post("/create", post)
       .then((res) => console.log(res))
       .catch((err) => console.log(err));
 
-     navigate("posts");
+    navigate("posts");
   };
-
-  useEffect(() => {
-    console.log('Count is now: ', post);
-  }, [post]);
-
 
   return (
     <div style={{ textAlign: "center", width: "90%", margin: "auto auto" }}>
-      <h1>CreatePost</h1>
-
+      <h1>Create post page</h1>
       <Form>
         <Form.Group>
           <Form.Control
-          name="title" 
-          placeholder='Title'
-          value={post.title}
-          onChange={handleChange}
-          style={{ marginBottom: "1rem" }}/>
-          <Form.Control 
-          name="description" 
-          placeholder='Description'
-          value={post.description}
-          onChange={handleChange}
-          style={{ marginBottom: "1rem" }}/>
+            name="title"
+            value={post.title}
+            onChange={handleChange}
+            style={{ marginBottom: "1rem" }}
+            placeholder="title"
+          />
           <Form.Control
-          name = "imgUrl"
-          style={{ marginBottom: "1rem" }}
-          accept="image/*"
-          type="file"
-          onChange={handleImgUrl}
-          required="required"/>
+            onChange={handleChange}
+            name="description"
+            value={post.description}
+            style={{ marginBottom: "1rem" }}
+            placeholder="description"
+          />
         </Form.Group>
-
-        <Button 
-        variant="outline-success"
-        style={{ width: "100%", marginBottom: "1rem" }}
-        onClick={createPost}>
-        CREATE POST 
+        <Button
+          onClick={createPost}
+          variant="outline-success"
+          style={{ width: "100%", marginBottom: "1rem" }}
+        >
+          CREATE POST
         </Button>
-
       </Form>
-
       <Button
-      variant="outline-dark"
-      style={{ width: "100%" }} 
-      onClick={() => navigate(-1)}>
-      BACK
+        onClick={() => navigate("posts")}
+        variant="outline-success"
+        style={{ width: "100%" }}
+      >
+        ALL POSTS
       </Button>
     </div>
-  )
+  );
 }
 
-export default CreatePost
+export default CreatePost;
