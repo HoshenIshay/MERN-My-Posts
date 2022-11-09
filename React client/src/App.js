3f60a7c04b50abaf6476ed1e83b4bff60d40fde2
Hoshen Ishay <87603302+HoshenIshay@users.noreply.github.com>
2022-11-09 19:38:18 +0200

    Update App.js

--- React client/src/App.js
+++ React client/src/App.js
@@ -1,26 +1,25 @@
-import './App.css';
+import "./App.css";
 import { Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import welcomeImg from './img/Welcome.png'
 
-
 function App() {
-
   const navigate = useNavigate();
 
   return (
-    <div style={{ width: "90%", margin: "auto auto" , textAlign: "center"}}>
-      <h1 style={ {marginTop: "1rem" }}>My Posts App</h1>
+    <div className="App">
+      <h1>Home page</h1>
       <img src={welcomeImg} alt="welcomeImg" />
       <Button
-      variant="outline-dark"
-      style={{ width: "100%" , marginBottom: "1rem"}} 
-      onClick={() => navigate("create")}>
-      NEW POST
+        variant="outline-success"
+        style={{ width: "90%" , marginBottom: "1rem" }}
+        onClick={() => navigate("create")}
+      >
+        NEXT
       </Button>
       <Button
       variant="outline-dark"
-      style={{ width: "100%" , marginBottom: "1rem"}} 
+      style={{ width: "90%" , marginBottom: "1rem"}} 
       onClick={() => navigate("/create/posts")}>
       POSTS
       </Button>
